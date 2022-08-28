5144cac7a0642c59f9773f7c3819076bdc579a2c
ahmed <ahmed.shalaby5125@gmail.com>
2022-08-28 13:48:31 +0200

    portfolio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.typing-name', {
+    strings: [' ahmed shalaby'],
+    typeSpeed:120,
+});
+var typed = new Typed('.typing-text', {
+    strings: [' front-End developer'],
+    typeSpeed:100,
+});
+var typed = new Typed('.typing-paragraph', {
+    strings: [' ahmed, Junior Web Developer, a passionate freelancer bringing you programming and design from the future. I am experienced in developing web.'],
+    typeSpeed:15,
+    
+});
+
